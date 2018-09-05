97685e45869d7086030bbf3534e493b4c0f52d3b
iGame3DBill <igame3dbill@gmail.com>
2018-09-05 07:10:31 -0600

    Simple Player Movement in 2D
    Simple player movement in 2D as single object or array of objects

---
+++ Assets/Scripts/SpriteMove2DArray.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMove2DArray : MonoBehaviour {
+    [SerializeField]
+    float moveSpeed;
+
+    // Data Type + [] makes array string[], int[], float[], bool[], GameObject[] etc.
+    [SerializeField]
+    Transform[] transforms;
+
+	// Use this for initialization
+	void Start () {
+       // transforms = this.gameObject.GetComponent<Transform>();	
+	}
+	
+	// Update is called once per frame
+        void FixedUpdate()
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+            float moveVertical = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        // move many objects at once
+        foreach (Transform T in transforms)
+        {
+            T.Translate(new Vector3(moveHorizontal, moveVertical));
+        }
+
+         
+        }
+    
+}
---
+++ Assets/Scripts/SpriteMoveSingle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMoveSingle : MonoBehaviour {
+    [SerializeField]
+    float moveSpeed;
+    [SerializeField]
+    Transform transform;
+
+	// Use this for initialization
+	void Start () {
+       transform = this.gameObject.GetComponent<Transform>();	
+	}
+	
+	// Update is called once per frame
+        void FixedUpdate()
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+            float moveVertical = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+            transform.Translate(new Vector3(moveHorizontal, moveVertical));
+        }
+    
+}
